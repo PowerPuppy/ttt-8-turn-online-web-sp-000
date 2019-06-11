c24e2328260317c74ee46499b5a2c56d90174a92
Chris Cheong <chrisxcheong@gmail.com>
2019-06-11 18:36:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,27 @@
-def turn(board)
-  puts "Please enter 1-9:"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board,index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def valid_move?(board,index)
+  if  index<8 && index>=0 && position_taken?(board,index) == false
+    return true
+  else
+    return false
+  end
 end
